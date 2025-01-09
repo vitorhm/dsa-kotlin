f614f34fe07e7bd11304208f4f909dd7e8660438
Vitor Messias <vitorhsm14@gmail.com>
2025-01-09 20:54:42 -0300

    Adiciona complexidade por espaço

--- src/main/kotlin/dshashtable/FirstRecurringCharacter.kt
+++ src/main/kotlin/dshashtable/FirstRecurringCharacter.kt
@@ -1,6 +1,25 @@
 package dshashtable
 
-fun find(array: Array<Int>): Int? {
+// Find recurring character with time > space
+fun findFavoringSpaceComplexity(array: Array<Int>): Int? {
+
+    for (i in array) {
+
+        for (x in i+1 until array.size) {
+            if (i == array[x]) {
+                return i
+            }
+        }
+    }
+
+    return null
+
+    // Time Complexity  -> O(n²) -> Nested Loop
+    // Space Complexity -> O(1)
+}
+
+// Find recurring character with space > time
+fun findFavoringTimeComplexity(array: Array<Int>): Int? {
 
     val hashSet = hashSetOf<Int>()
 
@@ -10,11 +29,17 @@ fun find(array: Array<Int>): Int? {
     }
 
     return null
+
+    // Time Complexity  -> O(n)
+    // Space Complexity -> O(n) -> We create a new hashset with the size of (n)
 }
 
 fun main() {
 
-    println(find(arrayOf(1, 2, 5, 4, 5, 3, 4, 5))) // 5
-    println(find(arrayOf(1, 2, 3, 4))) // null
+    println(findFavoringSpaceComplexity(arrayOf(1, 2, 5, 4, 5, 3, 4, 5))) // 5
+    println(findFavoringSpaceComplexity(arrayOf(1, 2, 3, 4))) // null
+
+    println(findFavoringTimeComplexity(arrayOf(1, 2, 5, 4, 5, 3, 4, 5))) // 5
+    println(findFavoringTimeComplexity(arrayOf(1, 2, 3, 4))) // null
 
 }
