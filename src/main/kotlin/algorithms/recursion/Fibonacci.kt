6ea1cac1374b458799fdd79aaa44102610d18fce
Vitor Messias <vitorhsm14@gmail.com>
2025-01-18 14:19:47 -0300

    Fibonacci iterative

--- src/main/kotlin/algorithms/recursion/Fibonacci.kt
+++ src/main/kotlin/algorithms/recursion/Fibonacci.kt
@@ -8,7 +8,27 @@ fun fibonacci(n: Int): Int {
     return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
+fun fibonacciIterative(n: Int): Int {
+
+    if (n < 2) return n
+
+    var n1 = 0
+    var n2 = 1
+    var current = 0
+
+    for (i in 2..n) {
+
+        current = n1 + n2
+        n1 = n2
+        n2 = current
+    }
+
+    return current
+    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+}
+
 fun main() {
 
-    println(fibonacci(4))
+    println(fibonacci(5))
+    println(fibonacciIterative(5))
 }
