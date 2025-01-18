ec85e220233310b098b651336fad33e54f0c2724
Vitor Messias <vitorhsm14@gmail.com>
2025-01-18 10:57:16 -0300

    Simplify

--- src/main/kotlin/algorithms/recursion/Factorial.kt
+++ src/main/kotlin/algorithms/recursion/Factorial.kt
@@ -1,23 +1,11 @@
 package algorithms.recursion
 
-fun factorial(n: Int, sum: Int = 0): Int {
-    if (n == 1) return sum
-    val s = if (sum > 0) sum * (n - 1) else n * (n - 1)
-    return factorial(n - 1, s)
+fun factorial(n: Int): Int {
+    if (n == 1) return n
+    return n * factorial(n - 1)
 }
 
-//fun factorial(n: Int, sum: Int): Int {
-//    if (n == 1) return sum
-//    val s = n * sum
-//    return factorial(n - 1, s)
-//}
-//
-//fun factorial(n: Int): Int {
-//    val sum = n * (n - 1)
-//    return factorial(n - 2, sum)
-//}
-
 fun main() {
-    val fact = factorial(5)
+    val fact = factorial(3)
     println(fact)
 }
